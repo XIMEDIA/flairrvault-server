8dd1ba1d5c84bfc936d6def9578f20f168274ee9
sopheapchea <sopheap@ximedialabs.com>
2020-09-28 12:07:15 -0400

    change db to mysql

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 const knex = require('knex')({
-  client: 'pg',
+  client: 'mysql',
   connection: {
-    host : process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user : process.env.DB_USER,
-    password : process.env.DB_PASS ? process.env.DB_PASS : '',
-    database : process.env.DB_NAME
+    host : process.env.DB_HOST_VAULT,
+    port: process.env.DB_PORT_VAULT,
+    user : process.env.DB_USER_VAULT,
+    password : process.env.DB_PASS_VAULT ? process.env.DB_PASS_VAULT : '',
+    database : process.env.DB_NAME_VAULT
   }
 });
 
